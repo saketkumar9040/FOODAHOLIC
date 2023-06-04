43a35d0ef4024a1bf1ac86f1ba1e68fcecf95b9c
saket kumar <saketk89@gmail.com>
2023-06-04 22:56:35 +0530

    feat - added flatlist in track order screen

--- src/firebase/FirebaseConfig.js
+++ src/firebase/FirebaseConfig.js
@@ -3,6 +3,23 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
+  apiKey: "AIzaSyA9PAsVzy6WAUBE_EiO5HCMBdBa8w9QMW8",
+  authDomain: "foodaholic-fd324.firebaseapp.com",
+  projectId: "foodaholic-fd324",
+  storageBucket: "foodaholic-fd324.appspot.com",
+  messagingSenderId: "104513043816",
+  appId: "1:104513043816:web:142d173b125fb356516add",
+  measurementId: "G-PQ6FYZ2DPC",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
+
+/*       OLD  -  FIREBASE - CREDENTIALS
+
   apiKey: "AIzaSyCgTyTM33fpogRnicjdfF9M3mug0ZU-izY",
   authDomain: "foodaholic-fd71f.firebaseapp.com",
   projectId: "foodaholic-fd71f",
@@ -10,10 +27,4 @@ const firebaseConfig = {
   messagingSenderId: "423529748469",
   appId: "1:423529748469:web:c9f01d9597d5b3a87fb2bc",
   measurementId: "G-Z0CB5GLVM5",
-};
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-export { firebase }
+*/
